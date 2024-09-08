b1a37d7c8e7c3dffa683f8601e4c3f06fbebef29
adi3489 <07aditya.cn@gmail.com>
2024-09-08 16:22:37 +0530

    card

--- App.js
+++ App.js
@@ -812,14 +812,9 @@ const Body = () => {
     <div classsName="body">
       <div className="search">Search</div>
       <div className="res-container">
-        <RestaurantCard resData={resList[0]} />
-        <RestaurantCard resData={resList[1]} />
-        <RestaurantCard resData={resList[2]} />
-        <RestaurantCard resData={resList[3]} />
-        <RestaurantCard resData={resList[4]} />
-        <RestaurantCard resData={resList[5]} />
-        <RestaurantCard resData={resList[6]} />
-        <RestaurantCard resData={resList[7]} />
+        {resList.map((restaurant) => (
+          <RestaurantCard key={restaurant.info.id} resData={restaurant} />
+        ))}
       </div>
     </div>
   );
